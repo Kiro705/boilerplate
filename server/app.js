333f4cf782cc55087e3d05b5219d18b0947d0632
Jackson Sui <kiro705@gmail.com>
2017-09-09 18:07:52 -0400

    started on express server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const {resolve} = require('path')
+const express = require('express');
+const app = express();
+
+// static middlewear
+app.use(express.static(resolve(__dirname, '..', 'public'))) // Serve static files from ../public
+app.use(express.static(resolve(__dirname, '..', 'node_modules')))
+
+//Other middlewear
+
+// Make sure this is right at the end of your server logic!
+// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// (However, if you have middleware to serve up 404s, that go would before this as well)
+app.get('*', function (req, res, next) {
+  res.sendFile(resolve(__dirname, './path/to/your/index.html'));
+});
+
+//
